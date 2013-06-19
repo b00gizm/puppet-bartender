b2dbfe10e58f29fe186a84dde3a5bad9439fbe32
b00gizm <b00gizm@gmail.com>
2013-06-19 22:54:25 +0200

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Bartender.app to /Applications.
+#
+# Examples
+#
+#   include bartender
+
+class bartender {
+  package { 'Bartender':
+    provider => 'compressed_app',
+    source   => 'http://www.macbartender.com/Demo/Bartender.zip',
+  }
 }
